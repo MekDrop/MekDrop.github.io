5c3f4dae0c33bee09c25fce382b5ab936e04591a
Raimondas Rimkevičius (aka MekDrop) <github@mekdrop.name>
2024-01-28 03:12:40 +0200

    Improved language autodetecting

--- src/assets/helpers/route.js
+++ src/assets/helpers/route.js
@@ -1,3 +1,6 @@
+import accepts from 'accepts'
+import normalizeUrl from 'normalize-url'
+
 export function getRouteUrl(to, router, ssrContext = null) {
   let url = router.resolve(to).href;
   if (url.startsWith('/')) {
@@ -5,13 +8,30 @@ export function getRouteUrl(to, router, ssrContext = null) {
   }
 
   if (ssrContext) {
-    url = '//' + ssrContext.req.headers.host + '/' + url;
+    url = ssrContext.req.protocol + '://' + ssrContext.req.headers.host + '/' + url;
   } else {
     url = window.location.origin + '/' + url;
   }
-  return url;
+  return normalizeUrl(url);
 }
 
-export function getCurrentLocaleFromRoute(to) {
-  return to.meta.autoSwitchLanguage ? to.params.lang : 'en-US'
+export function getCurrentLocaleFromRoute(to, ssrContext, availableLanguages) {
+  if (to.meta.autoSwitchLanguage && to.params.lang) {
+    return to.params.lang;
+  }
+
+  if (ssrContext) {
+    const accept = accepts(ssrContext.req);
+    const languages = accept.languages();
+
+    for (let i = 0; i < languages.length; i++) {
+      if (availableLanguages.includes(languages[i])) {
+        return languages[i];
+      }
+    }
+  } else if (typeof navigator !== 'undefined' && availableLanguages.includes(navigator.language)) {
+    return navigator.language
+  }
+
+  return 'en-US';
 }
