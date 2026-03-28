3b1e9c743668e53cca39a4393e5b760fa14fe6cc
MekDrop <342641+MekDrop@users.noreply.github.com>
2026-03-28 14:23:20 +0000

    Apply Prettier formatting

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,21 @@
-const { FlatCompat } = require('@eslint/eslintrc')
-const js = require('@eslint/js')
+const { FlatCompat } = require("@eslint/eslintrc");
+const js = require("@eslint/js");
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
-})
+});
 
 module.exports = [
   {
     ignores: [
-      'dist/**',
-      'src-capacitor/**',
-      'src-cordova/**',
-      '.quasar/**',
-      'node_modules/**',
-      '.eslintrc.js',
-      'quasar.config.*.temporary.compiled*',
+      "dist/**",
+      "src-capacitor/**",
+      "src-cordova/**",
+      ".quasar/**",
+      "node_modules/**",
+      ".eslintrc.js",
+      "quasar.config.*.temporary.compiled*",
     ],
   },
   ...compat.config({
@@ -26,26 +26,26 @@ module.exports = [
     env: {
       node: true,
       browser: true,
-      'vue/setup-compiler-macros': true,
+      "vue/setup-compiler-macros": true,
     },
-    extends: ['plugin:vue/vue3-essential', 'prettier'],
-    plugins: ['vue'],
+    extends: ["plugin:vue/vue3-essential", "prettier"],
+    plugins: ["vue"],
     globals: {
-      ga: 'readonly',
-      cordova: 'readonly',
-      __statics: 'readonly',
-      __QUASAR_SSR__: 'readonly',
-      __QUASAR_SSR_SERVER__: 'readonly',
-      __QUASAR_SSR_CLIENT__: 'readonly',
-      __QUASAR_SSR_PWA__: 'readonly',
-      process: 'readonly',
-      Capacitor: 'readonly',
-      chrome: 'readonly',
+      ga: "readonly",
+      cordova: "readonly",
+      __statics: "readonly",
+      __QUASAR_SSR__: "readonly",
+      __QUASAR_SSR_SERVER__: "readonly",
+      __QUASAR_SSR_CLIENT__: "readonly",
+      __QUASAR_SSR_PWA__: "readonly",
+      process: "readonly",
+      Capacitor: "readonly",
+      chrome: "readonly",
     },
     rules: {
-      'prefer-promise-reject-errors': 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-      'vue/no-v-text-v-html-on-component': 'off',
+      "prefer-promise-reject-errors": "off",
+      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+      "vue/no-v-text-v-html-on-component": "off",
     },
   }),
-]
+];
